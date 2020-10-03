daab12e6c28ba0174fe2a6ede2f9aed108909688
Zernova <zernova.anna1@gmail.com>
2020-10-03 10:54:09 +0300

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,10 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.time.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -24,16 +23,61 @@ public class UserMealsUtil {
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo.forEach(System.out::println);
 
-//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
+    //return filtered list with excess
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with excess. Implement by cycles
-        return null;
+
+        //count actual calories per day
+        Map<LocalDate,Integer> caloriesPerDayFact = new HashMap<>();
+
+        meals.forEach (
+            meal -> caloriesPerDayFact.merge(Year.of(meal.getDateTime().getYear()).atDay(meal.getDateTime().getDayOfYear()),
+                    meal.getCalories(), Integer::sum)
+        );
+
+        //filter and create new list with excess
+        List<UserMealWithExcess> mealsWithExcess = new ArrayList<>();
+
+        meals.forEach (
+            meal -> {
+                if (meal.getDateTime().toLocalTime().isAfter(startTime) && meal.getDateTime().toLocalTime().isBefore(endTime)) {
+                    if (caloriesPerDay < caloriesPerDayFact.get(meal.getDateTime().toLocalDate())) {
+                        mealsWithExcess.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), true));
+                    } else
+                        mealsWithExcess.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), false));
+                }
+            }
+        );
+
+        return mealsWithExcess;
     }
 
+    //return filtered list with excess
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO Implement by streams
-        return null;
+
+        //count actual calories per day
+        Map<LocalDate,Integer> caloriesPerDayFact = meals
+                .stream()
+                .collect(Collectors.toMap(meal->Year.of(meal.getDateTime().getYear()).atDay(meal.getDateTime().getDayOfYear()),
+                        UserMeal::getCalories, Integer::sum));
+
+        //stream with meal with excess
+        Stream stream1 = meals.stream()
+                .filter(meal -> (meal.getDateTime().toLocalTime().isAfter(startTime) && meal.getDateTime().toLocalTime().isBefore(endTime)))
+                .filter(meal -> caloriesPerDay < caloriesPerDayFact.get(meal.getDateTime().toLocalDate()))
+                .map(meal -> new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), true));
+
+        //stream with meal without excess
+        Stream stream2 = meals.stream()
+                .filter(meal -> (meal.getDateTime().toLocalTime().isAfter(startTime) && meal.getDateTime().toLocalTime().isBefore(endTime)))
+                .filter(meal -> caloriesPerDay >= caloriesPerDayFact.get(meal.getDateTime().toLocalDate()))
+                .map(meal -> new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), false));
+
+        Stream<UserMealWithExcess> streams = Stream.concat(stream1, stream2);
+
+        return streams.collect(Collectors.toList());
+
     }
 }
